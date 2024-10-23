9781ffef909a9599b3506261b1b23c9bab37adee
suju <bhagatsujal0@gmail.com>
2024-10-24 00:36:55 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./index.css";
+
+import App from "./App";
+import { Provider } from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/app/store.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+reportWebVitals();
